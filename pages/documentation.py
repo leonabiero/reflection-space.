a39c0e473faeeddd33fd3eee2a6f86de42a35a2a
leonabiero <leonabiero@gmail.com>
2026-07-25 06:41:58 +0300

    Update documentation.py

--- pages/documentation.py
+++ pages/documentation.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from services.draft_storage import save_draft
-from services.language import init_language, render_nav
-from services.visit_log import log_visit
+from services.language import init_language
+from navigation.router import render_nav
 from services.identity import init_identity, render_identity_footer
 
 T = init_language()
-log_visit("documentation", st.session_state.lang)
 user_name, user_role = init_identity(T)
 render_nav(T)
 render_identity_footer(T)
