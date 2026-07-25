ac14e38546cde8f82a36a2a29dbd00ef13880b2a
leonabiero <leonabiero@gmail.com>
2026-07-25 06:42:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from services.draft_storage import init_db
-from services.language import init_language, render_nav
-from services.visit_log import log_visit
+from services.language import init_language
+from navigation.router import render_nav
 from services.identity import init_identity, render_identity_footer
 
 st.set_page_config(page_title="Reflection Space", layout="centered")
 init_db()
 
 T = init_language()
-log_visit("home", st.session_state.lang)
 user_name, user_role = init_identity(T)
 render_nav(T)
 render_identity_footer(T)
